021d2187977786234fd9affb46ad7fa142a3fa5e
Nikita <n1ktrnv@gmail.com>
2021-10-05 22:02:14 +0300

    Добавляет комментарии для прогресс-бара

--- scanner/progressbar.py
+++ scanner/progressbar.py
@@ -1,6 +1,12 @@
 class ProgressBar:
 
     def __init__(self, total, suffix='', fill='█', width=40):
+        """
+        total: Количество операций, действий.
+        suffix: Сообщение, выводимое перед баром.
+        fill: Символ-заполнитель.
+        width: Кол-во символов, которое будет занимать бар.
+        """
         self._total = total
         self._fill = fill
         self._suffix = suffix
@@ -10,6 +16,9 @@ class ProgressBar:
         self._unused_part = 0
 
     def __str__(self):
+        """
+        Возвращает строковое представление бара в текущем состоянии.
+        """
         bar = '\r'
         if self._suffix:
             bar += self._suffix + ' '
@@ -24,32 +33,58 @@ class ProgressBar:
         return bar
 
     def __len__(self):
+        """
+        Возвращает длину строкового представления бара в текущем состоянии.
+        """
         return len(str(self))
 
     def print(self):
+        """
+        Печатает бар, убирая символ переноса для поодержки печати на одной
+        строке.
+        """
         print(self, end='')
 
     def update(self):
+        """
+        Переводит бар в следующее состояние и печатает его.
+        """
         self.next()
         self.print()
 
     def next(self):
+        """
+        Переводит бар в следующее состояние.
+        """
         if not self.completed():
             self._current += 1
 
     def completed(self):
+        """
+        Возвращает True, если бар находится в последнем возможном состоянии.
+        """
         return self._current == self._total
 
     @property
     def _completed_part(self):
+        """
+            Возвращает текущую долю завершенности всех операций.
+        """
         return self._current / self._total
 
     @property
     def _percentages(self):
+        """
+        Возвращает процент завершения всех операций.
+        """
         return round(self._completed_part * 100, 2)
 
     @property
     def _fill_count(self):
+        """
+        Возвращает кол-во символов, которое необходимо заполнить символом-
+        заполнителем.
+        """
         to_fill = self._completed_part * self._width
         if self._unused_part > 0:
             fill_count = round(to_fill + self._unused_part)
@@ -60,4 +95,8 @@ class ProgressBar:
 
     @property
     def _empty_count(self):
+        """
+        Возвращает кол-во символов, которое необходимо заполнить символом-
+        пустоты.
+        """
         return self._width - self._fill_count
