04bcef3b083efcd40a04dfb7e95c01eed5a45da7
Nikita <n1ktrnv@gmail.com>
2021-10-05 22:08:25 +0300

    Добавляет комментарии для сканнера портов

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -10,10 +10,17 @@ class PortsScanner:
     PORTS_COUNT = 2 ** 16
 
     def __init__(self, host):
+        """
+        host: Хост, который мы хотим отсканировать.
+        """
         self._host = host
         self._progress_bar = ProgressBar(self.PORTS_COUNT, 'Выполнено')
 
     def scan_ports(self, threads_count=None):
+        """
+        Сканирует порты: добавляет в пул на обработку потоки со всем портами.
+        threads_count: Максимальное кол-во потоков.
+        """
         self._progress_bar.print()
         with ThreadPoolExecutor(threads_count) as executor:
             lock = RLock()
@@ -21,10 +28,20 @@ class PortsScanner:
                 executor.submit(self._scan_port, port, lock)
 
     def info(self, message):
+        """
+        Корректно печатает сообщение в терминал при использовании бара.
+        message: Строка, которую нужно распечатать.
+        """
         print(message,
               end=(' ' * (len(self._progress_bar) - len(message)) + '\n'))
 
     def _scan_port(self, port, lock):
+        """
+        Сканирует один порт.
+        port: Порт.
+        lock: блокировка всех потоков. Используется, чтобы корректно напечатать
+        бар и информацию об открытом порте.
+        """
         with socket.socket() as sock:
             try:
                 sock.connect((self._host, port))
